f36846794bc72e6aa7f75261f791684d7fe25884
Gbil_Dye <71498299+DoSfwt1@users.noreply.github.com>
2026-05-08 16:24:52 +0200

    Update AUX.py

--- AUX.py
+++ AUX.py
@@ -4,8 +4,15 @@ import random
 import main.RES.UTS
 
 
+def SWP(LST, IN, OUT):
+    idx1 = target_list.index(val1)
+    idx2 = target_list.index(val2)
+    target_list[idx1], target_list[idx2] = target_list[idx2], target_list[idx1]
 def WIN_XAAB(list):
     for i in u:
         if list.index(i) in k:
+            SWP(list,i,list[random.choice(o)])
+            o.remove(list.index(i))
+    
             
             
