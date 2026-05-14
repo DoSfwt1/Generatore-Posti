67ce6d63f86d993f1261d710a7dc7026192439c8
Gbil_Dye <71498299+DoSfwt1@users.noreply.github.com>
2026-05-15 00:06:32 +0200

    Update Posti2.py

--- Posti2.py
+++ Posti2.py
@@ -58,11 +58,11 @@ def login_dialog():
 
 
 
-    if st.session_state.show_login == True:
-        if st.session_state.admin_mode == True:
-            st.error("Sei già dentro come amministratore!")
-    else:
-        login_dialog()
+if st.session_state.show_login == True:
+    if st.session_state.admin_mode == True:
+        st.error("Sei già dentro come amministratore!")
+ else:
+    login_dialog()
         
 
 
